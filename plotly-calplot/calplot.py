1424b21e2254a277c1ada92cfd6994a8cec5dcfe
Bruno Rodrigues <bruno.rsilva@g.globo>
2021-12-27 10:24:48 -0300

    mvp calplot

--- plotly-calplot/calplot.py
+++ plotly-calplot/calplot.py
@@ -5,23 +5,52 @@ from plotly import graph_objects as go
 from plotly.subplots import make_subplots
 
 
-def year_calplot(data: DataFrame, year, fig, row, month_lines:bool=True, colorscale:str="greens", title="", height=500):
-    month_names = list(data["ds"].dt.month_name().unique())
+def get_weeknumber_of_date(d):
+    """
+    Pandas week returns some strange values, this function fixes'em
+    """
+    if d.month == 1 and d.week > 50:
+        return 0
+    elif d.month == 12 and d.week < 10:
+        return 53
+    else:
+        return d.week
+
+
+def year_calplot(
+    data: DataFrame,
+    x,
+    y,
+    year,
+    fig,
+    row,
+    month_lines,
+    month_lines_width,
+    colorscale,
+    gap,
+    title,
+    blackout,
+    width,
+    each_plot_height=None,
+):
+    month_names = list(data[x].dt.month_name().unique())
     month_days = []
-    for month in data["ds"].dt.month.unique():
-        month_days.append(data.loc[data["ds"].dt.month == month].max()["ds"].day)
+    for month in data[x].dt.month.unique():
+        month_days.append(data.loc[data[x].dt.month == month].max()[x].day)
 
     month_positions = (np.cumsum(month_days) - 15) / 7
 
-    weekdays_in_year = [i.weekday() for i in data["ds"]]
+    weekdays_in_year = [i.weekday() for i in data[x]]
 
     # sometimes the last week of the current year conflicts with next year's january
     # pandas will give those weeks the number 52 or 53, but this is bad news for this plot
     # therefore we need a correction, for a more in-depth explanation check
     # https://stackoverflow.com/questions/44372048/python-pandas-timestamp-week-returns-52-for-first-day-of-year
-    weeknumber_of_dates = [
-        i.week if not (i.month == 1 and i.week > 50) else 0 for i in data["ds"]
-    ]
+
+    weeknumber_of_dates = (
+        data[x].apply(lambda x: get_weeknumber_of_date(x)).values.tolist()
+    )
+
     # 4cc417 green #347c17 dark green
     # colorscale = [[False, "#eeeeee"], [True, "#76cf63"]]
 
@@ -31,22 +60,24 @@ def year_calplot(data: DataFrame, year, fig, row, month_lines:bool=True, colorsc
         go.Heatmap(
             x=weeknumber_of_dates,
             y=weekdays_in_year,
-            z=data["y"],
-            xgap=3,  # this
-            ygap=3,  # and this is used to make the grid-like apperance
+            z=data[y],
+            xgap=gap,  # this
+            ygap=gap,  # and this is used to make the grid-like apperance
             showscale=False,
             colorscale=colorscale,
-            hovertemplate="%{customdata} <br>z=%{z}",
-            customdata=data["ds"].astype(str),
-            name=str(year)
+            hovertemplate="%{customdata} <br>{}=%{z} <br>x=%{x}",
+            customdata=data[x].astype(str),
+            name=str(year),
         )
     ]
 
     if month_lines:
         kwargs = dict(
-            mode="lines", line=dict(color="#9e9e9e", width=1), hoverinfo="skip"
+            mode="lines",
+            line=dict(color="#9e9e9e", width=month_lines_width),
+            hoverinfo="skip",
         )
-        for date, dow, wkn in zip(data["ds"], weekdays_in_year, weeknumber_of_dates):
+        for date, dow, wkn in zip(data[x], weekdays_in_year, weeknumber_of_dates):
             if date.day == 1:
                 cplt += [
                     go.Scatter(x=[wkn - 0.5, wkn - 0.5], y=[dow - 0.5, 6.5], **kwargs)
@@ -61,7 +92,38 @@ def year_calplot(data: DataFrame, year, fig, row, month_lines:bool=True, colorsc
                         ),
                     ]
 
-    if row == 0:
+    #    if row == 0:
+    if blackout:
+        layout = go.Layout(
+            title=title,
+            yaxis=dict(
+                showline=False,
+                showgrid=False,
+                zeroline=False,
+                tickmode="array",
+                ticktext=["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
+                tickvals=[0, 1, 2, 3, 4, 5, 6],
+                autorange="reversed",
+            ),
+            xaxis=dict(
+                showline=False,
+                showgrid=False,
+                zeroline=False,
+                tickmode="array",
+                ticktext=month_names,
+                tickvals=month_positions,
+            ),
+            font={"size": 10, "color": "#fff"},
+            paper_bgcolor=("#333"),
+            plot_bgcolor=("#333"),
+            margin=dict(t=20, b=20),
+            showlegend=False,
+        )
+        fig.update_layout(layout)
+        fig.update_xaxes(layout["xaxis"])
+        fig.update_yaxes(layout["yaxis"])
+
+    else:
         layout = go.Layout(
             title=title,
             yaxis=dict(
@@ -83,41 +145,74 @@ def year_calplot(data: DataFrame, year, fig, row, month_lines:bool=True, colorsc
             ),
             font={"size": 10, "color": "#9e9e9e"},
             plot_bgcolor=("#fff"),
-            margin=dict(t=40),
+            margin=dict(t=20, b=20),
             showlegend=False,
         )
-        fig.update_layout(layout)
-        fig.update_xaxes(layout["xaxis"])
-        fig.update_yaxes(layout["yaxis"])
-
+    fig.update_layout(layout)
+    fig.update_xaxes(layout["xaxis"])
+    fig.update_yaxes(layout["yaxis"])
+    fig.update_layout(width=width, height=each_plot_height)
     fig.add_traces(cplt, rows=[(row + 1)] * len(cplt), cols=[1] * len(cplt))
 
     return fig
 
 
-def fill_empty_with_zeros(selected_year_data: DataFrame, year: int):
+def fill_empty_with_zeros(selected_year_data: DataFrame, x, blackout, year: int):
     year_min_date = "01-01-" + str(year)
     year_max_date = "31-12-" + str(year)
-    df = pd.DataFrame({"ds": pd.date_range(year_min_date, year_max_date)})
+    df = pd.DataFrame({x: pd.date_range(year_min_date, year_max_date)})
     final_df = df.merge(selected_year_data, how="left")
-    final_df = final_df.fillna(0)
+    if not blackout:
+        final_df = final_df.fillna(0)
     return final_df
 
 
-def calplot(data: DataFrame, height=250):
-    unique_years = data["ds"].dt.year.unique()
+def calplot(
+    data: DataFrame,
+    x,
+    y,
+    blackout=False,
+    month_lines_width=1,
+    gap=3,
+    years_title=False,
+    width=800,
+    colorscale="greens",
+    title="",
+    month_lines=True,
+    total_height=1500,
+    space_between_plots=0.08,
+):
+    unique_years = data[x].dt.year.unique()
     unique_years_amount = len(unique_years)
-    fig = make_subplots(unique_years_amount, 1, subplot_titles=unique_years.astype(str))
+    if years_title:
+        subplot_titles = unique_years.astype(str)
+    else:
+        subplot_titles = None
+    fig = make_subplots(
+        unique_years_amount,
+        1,
+        subplot_titles=subplot_titles,
+        vertical_spacing=space_between_plots,
+    )
     for i, year in enumerate(unique_years):
-        selected_year_data = data.loc[data["ds"].dt.year == year]
-        selected_year_data = fill_empty_with_zeros(selected_year_data, year)
+        selected_year_data = data.loc[data[x].dt.year == year]
+        selected_year_data = fill_empty_with_zeros(selected_year_data, x, blackout, year)
 
-        year_calplot(selected_year_data, month_lines=True, year=year, fig=fig, row=i)
-        fig.update_layout(height=height * len(unique_years))
+        year_calplot(
+            selected_year_data,
+            x,
+            y,
+            month_lines=month_lines,
+            month_lines_width=month_lines_width,
+            colorscale=colorscale,
+            year=year,
+            fig=fig,
+            blackout=blackout,
+            width=width,
+            gap=gap,
+            title=title,
+            row=i,
+            each_plot_height=int(total_height / unique_years_amount),
+        )
 
     return fig
-
-
-def twelve_months_calplot():
-    pass
-
--- plotly-calplot/mock_example.py
+++ plotly-calplot/mock_example.py
@@ -1,7 +1,8 @@
 import numpy as np
 import pandas as pd
-from calplot import calplot
 import streamlit as st
+from calplot import calplot
+
 # mock setup
 dummy_start_date = "2019-01-01"
 dummy_end_date = "2021-10-03"
@@ -16,6 +17,14 @@ dummy_df = pd.DataFrame(
         ),
     }
 )
-fig = calplot(dummy_df)
+fig = calplot(
+    dummy_df,
+    x="ds",
+    y="y",
+    gap=0,
+    month_lines=False,
+    total_height=1000,
+    years_title=False,
+)
 
 st.plotly_chart(fig)

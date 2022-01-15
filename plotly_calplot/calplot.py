01154d98cd09c250bcec5e335e9c3ae107b213de
Bruno Rodrigues <bruno.rsilva@g.globo>
2022-01-15 01:13:32 -0300

    change how total_height is calculated

--- plotly_calplot/calplot.py
+++ plotly_calplot/calplot.py
@@ -170,27 +170,87 @@ def fill_empty_with_zeros(selected_year_data: DataFrame, x, dark_theme, year: in
 
 def calplot(
     data: DataFrame,
-    x,
-    y,
-    name="y",
-    dark_theme=False,
-    month_lines_width=1,
-    month_lines_color="#9e9e9e",
-    gap=1,
-    years_title=False,
-    width=800,
-    colorscale="greens",
-    title="",
-    month_lines=True,
-    total_height=1500,
-    space_between_plots=0.08,
+    x: str,
+    y: str,
+    name: str = "y",
+    dark_theme: bool = False,
+    month_lines_width: int = 1,
+    month_lines_color: str = "#9e9e9e",
+    gap: int = 1,
+    years_title: bool = False,
+    width: int = 800,
+    colorscale: str = "greens",
+    title: str = "",
+    month_lines: bool = True,
+    total_height: int = None,
+    space_between_plots: float = 0.08,
 ):
+    """
+    Yearly Calendar Heatmap
+
+    Parameters
+    ----------
+    data : DataFrame
+        Must contain at least one date like column and
+        one value column for displaying in the plot
+    
+    x : str
+        The name of the date like column in data
+
+    y : str
+        The name of the value column in data
+
+    dark_theme : bool = False
+        Option for creating a dark themed plot
+
+    month_lines: bool = True
+        if true will plot a separation line between
+        each month in the calendar
+
+    month_lines_width : int = 1
+        if month_lines this option controls the width of
+        the line between each month in the calendar
+
+    month_lines_color : str = "#9e9e9e"
+        if month_lines this option controls the color of
+        the line between each month in the calendar
+
+    gap : int = 1
+        controls the gap bewteen daily squares
+
+    years_title : bool = False
+        if true will add a title for each subplot with the
+        correspondent year
+
+    width : int = 800
+        controls the width of the plot
+
+    colorscale : str = "greens"
+        controls the colorscale for the calendar, works
+        with all the standard Plotly Colorscales and also
+        supports custom colorscales made by the user
+
+    title : str = ""
+        title of the plot
+
+    total_height : int = None
+        if provided a value, will force the plot to have a specific
+        height, otherwise the total height will be calculated
+        according to the amount of years in data
+    
+    space_between_plots: float = 0.08
+        controls the vertical space between the plots
+    """
     unique_years = data[x].dt.year.unique()
     unique_years_amount = len(unique_years)
     if years_title:
         subplot_titles = unique_years.astype(str)
     else:
         subplot_titles = None
+
+    if total_height is None:
+        total_height = 500 * unique_years_amount
+
     fig = make_subplots(
         unique_years_amount,
         1,

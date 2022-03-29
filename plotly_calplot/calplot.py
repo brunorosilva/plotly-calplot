8a2557bb3b9bcb340235a73dde5a44f10d8fe582
David Davó <david@ddavo.me>
2022-03-29 10:00:33 +0200

    Using date instead of str to remove UserWarnings

--- plotly_calplot/calplot.py
+++ plotly_calplot/calplot.py
@@ -1,3 +1,4 @@
+from datetime import date
 import numpy as np
 import pandas as pd
 from pandas.core.frame import DataFrame
@@ -154,8 +155,8 @@ def year_calplot(
 
 
 def fill_empty_with_zeros(selected_year_data: DataFrame, x, dark_theme, year: int):
-    year_min_date = "01-01-" + str(year)
-    year_max_date = "31-12-" + str(year)
+    year_min_date = date(year=year, month=1, day=1)
+    year_max_date = date(year=year, month=12, day=31)
     df = pd.DataFrame({x: pd.date_range(year_min_date, year_max_date)})
     final_df = df.merge(selected_year_data, how="left")
     if not dark_theme:
